bb4dd9dbeb7e11e96b3507b874adf8d6b6157f41
Omkar Lonkar <omkarlonkar294@gmail.com>
2024-03-08 12:46:15 -0800

    made a bundle, added SyncReadMem

--- src/main/scala/DSP228/RAM.scala
+++ src/main/scala/DSP228/RAM.scala
@@ -3,17 +3,24 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental.FixedPoint
 
-class RAM(N: Int) extends Module {
-    val io = IO(new Bundle {
-        val read = Input(Bool())
-        val in_valid = Input(Bool())
-        val addr1 = Input(UInt(log2Ceil(N).W))
-        val addr2 = Input(UInt(log2Ceil(N).W))
-        val out_valid = Output(Bool())
-        val out1 = Output(FixedPoint(32.W, 8.BP))
-        val out2 = Output(FixedPoint(32.W, 8.BP))
-    })
+class RAMUnitIO(NumEntries: Int) extends Bundle {
+    val read = Input(Bool())
+    val in_valid = Input(Bool())
+    val in_data1 = Input(FixedPoint(32.W, 8.BP))
+    val in_data2 = Input(FixedPoint(32.W, 8.BP))
+    val addr1 = Input(UInt(log2Ceil(NumEntries).W))
+    val addr2 = Input(UInt(log2Ceil(NumEntries).W))
+    val out_valid = Output(Bool())
+    val out1 = Output(FixedPoint(32.W, 8.BP))
+    val out2 = Output(FixedPoint(32.W, 8.BP))
+}
+
+class RAM(NumEntries: Int) extends Module {
+    val io = IO(new RAMUnitIO(NumEntries))
     // TODO: Implement Behavior
+    val mem1 = SyncReadMem(NumEntries, FixedPoint(32.W, 8.BP))
+    val mem2 = SyncReadMem(NumEntries, FixedPoint(32.W, 8.BP))
+
     io.out_valid := 0.B 
     io.out1 := 0.F(32.W, 8.BP)
     io.out2 := 0.F(32.W, 8.BP)
